5d12b8d7292d53f9baa1ba1c31ccc54ce3bb959f
thinkofher <piaskupiasku@gmail.com>
2019-11-12 22:02:04 +0100

    Changed app config to use PSQLConfig.

--- config.go
+++ config.go
@@ -1,9 +1,13 @@
 package main
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "secret_postgres"
-	dbname   = "goblog"
+import (
+	"github.com/thinkofher/go-blog/db"
 )
+
+var CONFIG = db.PSQLConfig{
+	Host:     "localhost",
+	Port:     5432,
+	User:     "postgres",
+	Password: "secret_postgres",
+	DBName:   "goblog",
+}
