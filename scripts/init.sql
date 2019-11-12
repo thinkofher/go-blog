8648fd18c5ea308aa59551dfb3492615fe9e09cd
thinkofher <piaskupiasku@gmail.com>
2019-11-12 14:46:21 +0100

    Created init.sql script for psql database and added scripts folder

---
+++ scripts/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE blog_user (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR (50) UNIQUE NOT NULL,
+    password VARCHAR (50) NOT NULL,
+    email VARCHAR (355) UNIQUE NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    last_login TIMESTAMP
+);
+
+CREATE TABLE post (
+    post_id SERIAL,
+    author_id SERIAL REFERENCES blog_user(user_id),
+    body TEXT NOT NULL,
+    upvotes INTEGER NOT NULL,
+    downvotes INTEGER NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    PRIMARY KEY (post_id, author_id)
+);
