b4041e23f12dd3c3c909c7b1ede6414a6a0c20d3
thinkofher <piaskupiasku@gmail.com>
2019-11-15 13:28:21 +0100

    Implemented authentication for logged in users and added docs.

--- app/auth.go
+++ app/auth.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gorilla/sessions"
 )
 
+// AuthenticationMiddleware returns MiddlewareFunc, which uses
+// given CookieStore to authenticate is user is logged in.
 func AuthenticationMiddleware(store *sessions.CookieStore) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +20,6 @@ func AuthenticationMiddleware(store *sessions.CookieStore) mux.MiddlewareFunc {
 
 			_, ok := session.Values[userCookieKey]
 
-			// _, ok := userCookie.(db.PublicUserData)
 			if !ok {
 				session.AddFlash("You have to be logged in.")
 				err = session.Save(r, w)
@@ -27,6 +28,34 @@ func AuthenticationMiddleware(store *sessions.CookieStore) mux.MiddlewareFunc {
 					return
 				}
 				http.Redirect(w, r, "/login", http.StatusFound)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+// NonUsersOnly returns mux.MiddlewareFunc, which uses
+// given CookieStore to authenticate is user is logged out.
+func NonUsersOnly(store *sessions.CookieStore) mux.MiddlewareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			session, err := store.Get(r, SessionName)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			_, ok := session.Values[userCookieKey]
+
+			if ok {
+				session.AddFlash("You have to be logged out.")
+				err = session.Save(r, w)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				http.Redirect(w, r, "/index", http.StatusFound)
 				return
 			}
 			next.ServeHTTP(w, r)
