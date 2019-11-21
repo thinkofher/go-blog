476b1eb657176dd02a00c35fea1262ec49bb9799
thinkofher <piaskupiasku@gmail.com>
2019-11-21 09:30:29 +0100

    Divided main function and created router function for crating server router.

--- main.go
+++ main.go
@@ -6,20 +6,15 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
 	_ "github.com/lib/pq"
 
-	"github.com/thinkofher/go-blog/app"
-	"github.com/thinkofher/go-blog/app/index"
-	"github.com/thinkofher/go-blog/app/login"
-	"github.com/thinkofher/go-blog/app/posts"
-	"github.com/thinkofher/go-blog/app/register"
-	"github.com/thinkofher/go-blog/app/user"
 	"github.com/thinkofher/go-blog/db"
 )
 
 var store *sessions.CookieStore
+var dbwrapper db.Wrapper
+var err error
 
 func init() {
 	store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
@@ -30,7 +25,7 @@ func init() {
 
 func main() {
 
-	dbwrapper, err := db.NewWrapper(DBCONFIG)
+	dbwrapper, err = db.NewWrapper(DBCONFIG)
 	if err != nil {
 		panic(err)
 	}
@@ -38,36 +33,7 @@ func main() {
 
 	log.Println("Connection to database succesfull!")
 
-	fs := http.StripPrefix("/static/",
-		http.FileServer(http.Dir("./static/")))
-
-	r := mux.NewRouter()
-	r.PathPrefix("/static/").Handler(fs).Methods("GET")
-	r.Path("/logout").HandlerFunc(app.Logout(store, APPCONFIG)).Methods("GET")
-
-	nonUsers := r.PathPrefix("").Subrouter()
-
-	nonUsers.Path("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/login", http.StatusFound)
-	})
-	nonUsers.Path("/login").Handler(login.NewHandler(dbwrapper, store, APPCONFIG)).Methods("GET", "POST")
-	nonUsers.Path("/register").Handler(
-		register.NewHandler(dbwrapper, store, APPCONFIG)).Methods("GET", "POST")
-	nonUsers.Use(app.NonUsersOnly(store, APPCONFIG))
-
-	auth := r.PathPrefix("/index").Subrouter()
-
-	auth.Path("").Handler(index.NewHandler(dbwrapper, store, APPCONFIG)).Methods("GET")
-	auth.Use(app.AuthenticationMiddleware(store, APPCONFIG))
-
-	postsHandlers := r.PathPrefix("/post").Subrouter()
-	postsHandlers.Path("/new").HandlerFunc(posts.NewPost(dbwrapper, store, APPCONFIG)).Methods("POST")
-	postsHandlers.Path("/delete/{id:[0-9]+}").HandlerFunc(posts.RemovePost(dbwrapper, store, APPCONFIG))
-	postsHandlers.Use(app.AuthenticationMiddleware(store, APPCONFIG))
-
-	usersHandlers := r.PathPrefix("/user").Subrouter()
-	usersHandlers.Path("/{id:[0-9]+}").Handler(user.NewHandler(dbwrapper, store, APPCONFIG)).Methods("GET")
-	usersHandlers.Use(app.AuthenticationMiddleware(store, APPCONFIG))
+	r := router()
 
 	log.Println("Starting application at port ':8080'.")
 	log.Fatal(http.ListenAndServe(":8080", r))
---
+++ router.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/thinkofher/go-blog/app"
+	"github.com/thinkofher/go-blog/app/index"
+	"github.com/thinkofher/go-blog/app/login"
+	"github.com/thinkofher/go-blog/app/posts"
+	"github.com/thinkofher/go-blog/app/register"
+	"github.com/thinkofher/go-blog/app/user"
+)
+
+func router() *mux.Router {
+
+	fs := http.StripPrefix("/static/",
+		http.FileServer(http.Dir("./static/")))
+
+	r := mux.NewRouter()
+	r.
+		PathPrefix("/static/").
+		Handler(fs).
+		Methods("GET")
+	r.
+		Path("/logout").
+		HandlerFunc(app.Logout(store, APPCONFIG)).
+		Methods("GET")
+
+	nonUsers := r.PathPrefix("").Subrouter()
+	nonUsers.
+		Path("/").
+		HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "/login", http.StatusFound)
+		})
+	nonUsers.
+		Path("/login").
+		Handler(login.NewHandler(dbwrapper, store, APPCONFIG)).
+		Methods("GET", "POST")
+	nonUsers.
+		Path("/register").
+		Handler(register.NewHandler(dbwrapper, store, APPCONFIG)).
+		Methods("GET", "POST")
+	nonUsers.
+		Use(app.NonUsersOnly(store, APPCONFIG))
+
+	indexHandlers := r.PathPrefix("/index").Subrouter()
+	indexHandlers.
+		Path("").
+		Handler(index.NewHandler(dbwrapper, store, APPCONFIG)).
+		Methods("GET")
+	indexHandlers.
+		Use(app.AuthenticationMiddleware(store, APPCONFIG))
+
+	postsHandlers := r.PathPrefix("/post").Subrouter()
+	postsHandlers.
+		Path("/new").
+		HandlerFunc(posts.NewPost(dbwrapper, store, APPCONFIG)).
+		Methods("POST")
+	postsHandlers.
+		Path("/delete/{id:[0-9]+}").
+		HandlerFunc(posts.RemovePost(dbwrapper, store, APPCONFIG)).
+		Methods("POST")
+	postsHandlers.
+		Use(app.AuthenticationMiddleware(store, APPCONFIG))
+
+	usersHandlers := r.PathPrefix("/user").Subrouter()
+	usersHandlers.
+		Path("/{id:[0-9]+}").
+		Handler(user.NewHandler(dbwrapper, store, APPCONFIG)).
+		Methods("GET")
+	usersHandlers.
+		Use(app.AuthenticationMiddleware(store, APPCONFIG))
+
+	return r
+}
